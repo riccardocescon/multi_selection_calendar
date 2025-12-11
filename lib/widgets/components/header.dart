eb2e2c36ec474f7d9f0dd8f6258bf2c8adadedb6
Riccardo Cescon <r.cescon@keyautomation.it>
2025-12-11 09:37:47 +0100

    Added animation on header arrow icons

--- lib/widgets/components/header.dart
+++ lib/widgets/components/header.dart
@@ -1,6 +1,8 @@
 part of '../multi_selection_calendar.dart';
 
-class _Header extends StatelessWidget {
+enum _OpenedMenu { year, month, none }
+
+class _Header extends StatefulWidget {
   const _Header({
     required this.selectedYear,
     required this.monthIndex,
@@ -30,6 +32,13 @@ class _Header extends StatelessWidget {
   final HeaderDecoration decoration;
 
   @override
+  State<_Header> createState() => _HeaderState();
+}
+
+class _HeaderState extends State<_Header> {
+  _OpenedMenu _openedMenu = _OpenedMenu.none;
+
+  @override
   Widget build(BuildContext context) {
     return Row(
       children: [
@@ -37,41 +46,45 @@ class _Header extends StatelessWidget {
           child: Row(
             spacing: 16,
             children: [
-              GestureDetector(
-                behavior: HitTestBehavior.translucent,
-                onTap: onChangeMonth,
-                child: Row(
-                  spacing: 8,
-                  children: [
-                    Text(
-                      (decoration.shortMonthName
-                              ? DateFormat.MMM()
-                              : DateFormat.MMMM())
-                          .format(DateTime(selectedYear, monthIndex + 1)),
-                      style:
-                          decoration.monthTextStyle ??
-                          Theme.of(context).textTheme.headlineSmall,
-                    ),
-                    Icon(Icons.arrow_drop_down_rounded),
-                  ],
+              _AnimatedHeaderOption(
+                onTap: () {
+                  setState(() {
+                    _openedMenu = _openedMenu == _OpenedMenu.month
+                        ? _OpenedMenu.none
+                        : _OpenedMenu.month;
+                  });
+                  widget.onChangeMonth.call();
+                },
+                isOpened: _openedMenu == _OpenedMenu.month,
+                child: Text(
+                  (widget.decoration.shortMonthName
+                          ? DateFormat.MMM()
+                          : DateFormat.MMMM())
+                      .format(
+                        DateTime(widget.selectedYear, widget.monthIndex + 1),
+                      ),
+                  style:
+                      widget.decoration.monthTextStyle ??
+                      Theme.of(context).textTheme.headlineSmall,
                 ),
               ),
-              GestureDetector(
-                behavior: HitTestBehavior.translucent,
-                onTap: onChangeYear,
-                child: Row(
-                  spacing: 8,
-                  children: [
-                    Text(
-                      DateFormat.y().format(
-                        DateTime(selectedYear, monthIndex + 1),
-                      ),
-                      style:
-                          decoration.yearTextStyle ??
-                          Theme.of(context).textTheme.bodyLarge,
-                    ),
-                    Icon(Icons.arrow_drop_down_rounded),
-                  ],
+              _AnimatedHeaderOption(
+                onTap: () {
+                  setState(() {
+                    _openedMenu = _openedMenu == _OpenedMenu.year
+                        ? _OpenedMenu.none
+                        : _OpenedMenu.year;
+                  });
+                  widget.onChangeYear.call();
+                },
+                isOpened: _openedMenu == _OpenedMenu.year,
+                child: Text(
+                  DateFormat.y().format(
+                    DateTime(widget.selectedYear, widget.monthIndex + 1),
+                  ),
+                  style:
+                      widget.decoration.yearTextStyle ??
+                      Theme.of(context).textTheme.bodyLarge,
                 ),
               ),
             ],
@@ -80,18 +93,105 @@ class _Header extends StatelessWidget {
         Row(
           children: [
             IconButton(
-              onPressed: enablePrevMonth ? loadPrevMonth : null,
+              onPressed: widget.enablePrevMonth ? widget.loadPrevMonth : null,
               icon: const Icon(Icons.arrow_back_ios_new_rounded),
-              iconSize: decoration.iconSize,
+              iconSize: widget.decoration.iconSize,
             ),
             IconButton(
-              onPressed: enableNextMonth ? loadNextMonth : null,
+              onPressed: widget.enableNextMonth ? widget.loadNextMonth : null,
               icon: const Icon(Icons.arrow_forward_ios_rounded),
-              iconSize: decoration.iconSize,
+              iconSize: widget.decoration.iconSize,
             ),
           ],
         ),
       ],
+    );
+  }
+}
+
+class _AnimatedHeaderOption extends StatefulWidget {
+  const _AnimatedHeaderOption({
+    required this.onTap,
+    required this.isOpened,
+    required this.child,
+  });
+
+  final VoidCallback onTap;
+  final bool isOpened;
+  final Widget child;
+
+  @override
+  State<_AnimatedHeaderOption> createState() => _AnimatedHeaderOptionState();
+}
+
+class _AnimatedHeaderOptionState extends State<_AnimatedHeaderOption>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _rotation;
+
+  bool _isSelected = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 400),
+    );
+
+    _rotation = Tween<double>(
+      begin: 0,
+      end: 0.5,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
+  }
+
+  @override
+  void didUpdateWidget(covariant _AnimatedHeaderOption oldWidget) {
+    if (widget.isOpened != oldWidget.isOpened) {
+      if (widget.isOpened) {
+        _controller.forward();
+      } else {
+        _controller.reverse();
+      }
+    }
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _handleTap() {
+    setState(() {
+      _isSelected = !_isSelected;
+      if (_isSelected) {
+        _controller.forward();
+      } else {
+        _controller.reverse();
+      }
+    });
+
+    widget.onTap.call();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      behavior: HitTestBehavior.translucent,
+      onTap: _handleTap,
+      child: Row(
+        spacing: 8,
+        children: [
+          widget.child,
+          RotationTransition(
+            turns: _rotation,
+            child: const Icon(Icons.arrow_drop_down_rounded),
+          ),
+        ],
+      ),
     );
   }
 }
