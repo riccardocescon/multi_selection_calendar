855aa7b7164e830e5d4a416798f6bd77cff1652a
Riccardo Cescon <r.cescon@keyautomation.it>
2025-12-17 10:54:16 +0100

    added library

---
+++ lib/multi_calendar_selection.dart
@@ -0,0 +1,9 @@
+library;
+
+export 'widgets/multi_selection_calendar.dart';
+export 'widgets/calendar_day_background.dart';
+export 'controller/calendar_controller.dart';
+export 'entities/header_decoration.dart';
+export 'entities/calendar_decorations.dart';
+export 'entities/calendar_animation_settings.dart';
+export 'entities/calendar_picker_decoration.dart';
